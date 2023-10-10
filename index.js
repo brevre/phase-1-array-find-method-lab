4b4887c12b8d4eab9ce63479fc96a26676fc7340
brevre <brendamakena09@gmail.com>
2023-10-10 10:42:44 +0200

    completed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // code your solution here
+function superbowlWin(record) {
+    const winningYear = record.find(entry => entry.result === "W");
+  
+    if (winningYear) {
+      return winningYear.year;
+    } else {
+      return undefined;
+    }
+  }
+  
